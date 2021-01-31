61b20147cbd74547179d758616b4e6f6e237731b
joemacko <joemacko58@gmail.com>
2021-01-31 18:49:10 -0500

    Coded along through Eleven Note 4.03 API Controller-2

---
+++ ElevenNoteTwo.WebAPI/Controllers/NoteTwoController.cs
@@ -0,0 +1,43 @@
+﻿using ElevenNoteTwo.Models;
+using ElevenNoteTwo.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNoteTwo.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteTwoController : ApiController
+    {
+        private NoteTwoService CreateNoteTwoService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteTwoService(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            NoteTwoService noteService = CreateNoteTwoService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        public IHttpActionResult Post(NoteTwoCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteTwoService();
+
+            if (!service.CreateNoteTwo(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
