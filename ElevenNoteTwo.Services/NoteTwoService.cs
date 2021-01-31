47fee8310651abf8779aafbd468482cc53c29cc2
joemacko <joemacko58@gmail.com>
2021-01-31 17:47:01 -0500

    Coded along through Eleven Note 4.02 NoteService-2

---
+++ ElevenNoteTwo.Services/NoteTwoService.cs
@@ -0,0 +1,60 @@
+﻿using ElevenNoteTwo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevenNoteTwo.Models;
+
+namespace ElevenNoteTwo.Services
+{
+    public class NoteTwoService
+    {
+        private readonly Guid _userId;
+
+        public NoteTwoService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNoteTwo(NoteTwoCreate model)
+        {
+            var entity =
+                new NoteTwo()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteTwoListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteTwoListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
